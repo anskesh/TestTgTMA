454c524222acf4127f15e1fcaa8631aba53ce30e
anskesh <72143945+anskesh@users.noreply.github.com>
2025-02-27 16:47:38 +0000

    Deploying to gh-pages from @ anskesh/TestTgTMA@9860a1700eb70635ae936f31dc6dbd6c9a4f975b 🚀

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,36 @@
-﻿window.getElementHeight = (element) => {
+﻿window.messagesPreloaded = function () {
+    let loading = document.getElementById("loading");
+
+    loading.style.opacity = "0";
+    setTimeout(() => {
+        loading.style.display = "none";
+    }, 100);
+}
+
+window.getElementHeight = (element) => {
     return element ? element.offsetHeight : 0;
+}
+
+window.scrollHandler = {
+    init: function (sliderSelector, messageSelector, dotNetHelper) {
+        const slider = document.querySelector("." + sliderSelector);
+        if (!slider) return;
+        
+        let lastIndex = -1;
+        
+        slider.addEventListener('scroll', function (event) {
+            const messages = slider.querySelectorAll("." + messageSelector);
+            const sliderHeight = slider.clientHeight;
+            
+            messages.forEach((message, index) => {
+                const rect = message.getBoundingClientRect();
+                const isVisible = rect.top > 0 && rect.top < sliderHeight;
+                
+                if (isVisible && lastIndex !== index) {
+                    lastIndex = index;
+                    dotNetHelper.invokeMethodAsync("OnScrollIndexChanged", index);
+                }
+            })
+        })
+    }
 }
